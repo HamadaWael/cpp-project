034b7376f342bbf43580bbb89e9f51d0f84af2de
Mohamed Wael <hamo50994@gmail.com>
2024-09-12 11:40:37 +0300

    Update main (2).cpp

--- main (2).cpp
+++ main (2).cpp
@@ -1,27 +1,39 @@
 #include <iostream>
-#include <cmath>
 using namespace std;
 
 int main()
 {
-int Alice=0 , Bob=0 , Charlie=0;
+int person1=0 , person2=0 , person3=0;
 int num_person , Votes , i_1 , i_2 ;
 string person[4];
+string person_1 , person_2 , person_3 ;
+
+cout<<"enter your Names of candidates \n";
+cout<<"***************\n";
+cin >>person_1  ;
+cin >>person_2  ;
+cin >>person_3  ;
+cout<<"***************\n";
 
 cout<<"enter your num_person\n"; 
 cin >>num_person;
 cout<<"enter your Vote\n"; 
 cin >>Votes;
-
+cout<<"\n";
 cout<<"*******************\n";
-cout<<"[1] Alice"  <<"\n";          
-cout<<"[2] Bob"    <<"\n";          
-cout<<"[3] Charlie"<<"\n";          
+cout<<"["<< person_1 <<"]"<<"\n";          
+cout<<"["<< person_2 <<"]"<<"\n";          
+cout<<"["<< person_3 <<"]"<<"\n";          
 cout<<"*******************\n";
-
 
 for(int i_1=1 ; i_1<=num_person ; i_1++)
 {
+cout<<"____________________\n";    
+cout<<"*******************\n";
+cout<<"["<< person_1 <<"]"<<"\n";          
+cout<<"["<< person_2 <<"]"<<"\n";          
+cout<<"["<< person_3 <<"]"<<"\n";          
+cout<<"*******************\n";
 cout<<"Vote person num : "<<i_1<<"\n";    
 cout<<"***************\n";
 for(int i_2=1 ; i_2<=Votes ; i_2++)
@@ -30,41 +42,42 @@ cout<<"Rank"<<i_2<<" : ";
 //cin.ignore (1 , '\n');
 //getline (cin , person[i_2]);
 cin>>person[i_2];
-if     (person[1] == "Alice")
-        Alice+=3;   
-else if(person[2] == "Alice")
-        Alice+=2;   
-else if(person[3] == "Alice")
-        Alice++;   
-else if(person[1] == "Bob")     
-        Bob+=3;
-else if(person[2] == "Bob")     
-        Bob+=2;
-else if(person[3] == "Bob")     
-        Bob++;
-else if(person[1] == "Charlie")
-        Charlie+=3;
-else if(person[2] == "Charlie")
-        Charlie+=2;
-else if(person[3] == "Charlie")
-        Charlie++;
+if     (person[1] == person_1)
+        person1+=3;   
+else if(person[2] == person_1)
+        person1+=2;   
+else if(person[3] == person_1)
+        person1++;   
+else if(person[1] == person_2)     
+        person2+=3;
+else if(person[2] == person_2)     
+        person2+=2;
+else if(person[3] == person_2)     
+        person2++;
+else if(person[1] == person_3)
+        person3+=3;
+else if(person[2] == person_3)
+        person3+=2;
+else if(person[3] == person_3)
+        person3++;
 else   
         cout<<"unavailable\n";     
 }
 cout<<"***************\n";
+cout<<"____________________\n";    
 }
 
-if     (Alice>=Bob & Alice>=Charlie)
+if     (person1>=person2 & person1>=person3)
 {
-    cout<<"Alice is winner";
+    cout<< person_1 <<" is winner";
 }
-else if(Bob>=Alice & Bob>=Charlie) 
+else if(person2>=person1 & person2>=person3) 
 {
-    cout<<"Bob is winner";
+    cout<< person_2 <<" is winner";
 }
-else if(Charlie>=Alice & Charlie>=Bob)
+else if(person3>=person1 & person3>=person2)
 {
-    cout<<"Charlie is winner";
+    cout<< person_3 <<" is winner";
 }
 else
 {
